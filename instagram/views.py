0f473d1c781348c1bde94682f57e4e435b574f17
Koech-code <nixon.koech@moringaschool.com>
2021-09-12 01:42:00 +0300

    create a view for the created urls

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from .models import Image, Profile
 
 # Create your views here.
 def index(request):
+    images = Image.objects.all()
 
-    return render(request,'registration/index.html')
+    return render(request,"app/index.html", {"images":images})
+
+def profile(request):
+
+    return render(request,"app/profile.html")
+
+def search(request):
+
+    return render(request,"app/search.html")
+
+
