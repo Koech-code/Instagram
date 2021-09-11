c93d8adb9812ddbcff4ced5ee7f9de76386ccf7d
Koech-code <nixon.koech@moringaschool.com>
2021-09-12 01:39:55 +0300

    create a url for search and profile

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import url
 from . import views
 from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns=[
     url('^$', views.index, name='home'),
+    url('profile', views.profile, name='profile'),
+    url('search', views.search, name='search'),
 
 ]
 
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
