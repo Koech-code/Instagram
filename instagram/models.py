33823a520b50fb70fa5c1f2d41e436ad5f458241
Koech-code <nixon.koech@moringaschool.com>
2021-09-12 16:13:59 +0300

    modify the profile model by adding the user, followers and following columns

--- instagram/models.py
+++ instagram/models.py
@@ -1,11 +1,15 @@
 
 from django.db import models
 from django.db.models.deletion import CASCADE
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
     photo=models.ImageField(upload_to='images')
     bio=models.TextField(max_length=1200)
+    user=models.OneToOneField(User, on_delete=models.CASCADE, null=True, related_name='user')
+    following=models.ManyToManyField(User, related_name='following')
+    followers=models.ManyToManyField(User, related_name='followers')
 
 
 class Image(models.Model):
