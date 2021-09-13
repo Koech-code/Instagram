1ec3c5b4eab68ffca9dc85deaf946994d1b88575
Koech-code <nixon.koech@moringaschool.com>
2021-09-13 20:10:14 +0300

    create some test for the profile model

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+from .models import Profile, Image, Following
 # Create your tests here.
+class TestProfileClass(TestCase):
+    def setUp(self):
+        self.user=User(username='kipkorir')
+        self.user.save()
+
+        self.profile_test = Profile(id=1, photo='home.jpg', bio='developer',
+                                    user=self.user)
