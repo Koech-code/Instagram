dc3e82f14c98bcfa80ba54825a3f66d27e1bc48a
Koech-code <nixon.koech@moringaschool.com>
2021-09-13 20:07:38 +0300

    add a like and follow urls

--- instagram/urls.py
+++ instagram/urls.py
@@ -4,12 +4,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.views import LogoutView
 
+
 urlpatterns=[
     url('^$', views.index, name='home'),
-    url('logout/', LogoutView.as_view(), {"next_page": ''}),
+    url('logout/', LogoutView.as_view(), {"next_page": ''}),    
     url('profile/', views.profile, name='profile'),
     url('search/', views.search, name='search'),
+    url('follow/(\d+)',views.following,name='follow'),
+    url('like/(\d+)',views.like,name='like'),
+
 ]
-
-
-
+if settings.DEBUG == True:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
