8c6d5b414479cf87fef0048d98da5307dc2084f6
Koech-code <nixon.koech@moringaschool.com>
2021-09-13 19:58:59 +0300

    add the media root

--- igProject/settings.py
+++ igProject/settings.py
@@ -139,6 +139,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL='/'
+LOGOUT_REDIRECT_URL='/'
 
 SITE_ID=1
 
