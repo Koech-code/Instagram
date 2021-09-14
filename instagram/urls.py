998e39c31c88b887f2811a57baef90999221e7a7
Koech-code <nixon.koech@moringaschool.com>
2021-09-14 17:41:03 +0300

    change the likes url

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -9,10 +10,10 @@ urlpatterns=[
     url('^$', views.index, name='home'),
     url('logout/', LogoutView.as_view(), {"next_page": ''}),    
     url('profile/', views.profile, name='profile'),
-    url('search/', views.search, name='search'),
+    url('search/', views.search_results, name='search'),
     url('follow/(\d+)',views.following,name='follow'),
     url('like/(\d+)',views.like,name='like'),
 
 ]
-if settings.DEBUG == True:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
