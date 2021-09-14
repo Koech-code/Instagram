035e44d98970581ded00480558ff44b314e68641
Koech-code <nixon.koech@moringaschool.com>
2021-09-14 17:37:25 +0300

    register the following model

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Image, Profile
+from .models import Image, Profile, Following
 
 # Register your models here.
 admin.site.register(Image)
 admin.site.register(Profile)
+admin.site.register(Following)
